7dda63897002da713cbf929bb6a0b64ed75022dc
Akash Jaiswal <41293193+SkyThonk@users.noreply.github.com>
2020-08-26 19:27:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import keep_alive
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+client = MyClient()
+
+keep_alive.keep_alive()
+token = os.environ.get("Token")
+client.run(token)
