94f4a86d7ba7b98fb906d10ff5d7647b92b1f056
skyThonk <skydev777@gmail.com>
2020-09-20 22:12:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ class MyClient(discord.Client):
         if message.content.lower() == 'saveid':
             qsql.insert_game_id(message.author.id)
             emd = discord.Embed(title="Game ID",description = "Record Created Suscessfully", color = 0x00ff00)
-            emd.add_feild(name = "Valorant", value="Please enter",inline = False)
+            emd.add_field(name = "Valorant", value="Please enter",inline = False)
             await message.channel.send(embed = emd)
         
-        
+        if message.content.lower() == 'deleteid':
+            qsql.dele()
         # if message.channel.id == 428432032276938753 or message.channel.id == 747078225372643399:
         #     qsql.insert_data(message.content)
         #     await message.channel.send('data saved suscess')
